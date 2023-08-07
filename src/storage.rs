5cead8214fa8254211d50f098d895a5c4bb2ff0c
Robbie Coomber <robbie.coomber@gmail.com>
2023-08-07 08:17:26 +0100

    WIP read_all

--- src/storage.rs
+++ src/storage.rs
@@ -1,12 +1,13 @@
 use std::path::PathBuf;
-use std::u8;
+use std::{u8, io};
 
 use anyhow::Context;
+use anyhow::__private::kind::TraitKind;
 
 use crate::data::{get_max, get_min};
 use crate::metadata::{ColumnMetaData, TableMetaData};
 use crate::{get_dtype, DValue, DType};
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Result, Error};
 use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::io::ErrorKind;
@@ -49,7 +50,7 @@ impl IndexValue {
         };
     }
 
-    fn from_bytes(bytes: &[u8], dtype: DType) -> IndexValue {
+    fn from_bytes(bytes: &[u8], dtype: &DType) -> IndexValue {
         match dtype {
             DType::String => {
                 let mut s_bytes = [0; 8];
@@ -70,27 +71,33 @@ impl IndexValue {
 
 struct IndexEntry {
     start_position: IndexSize, // stored as 8 bytes big endian
-    size: IndexSize, // stored as 8 bytes big endian. In reality you don't need this but it's much simpler if you have it.
+    compressed_size: IndexSize, // stored as 8 bytes big endian. In reality you don't need this but it's much simpler if you have it.
+    decompressed_size: IndexSize, // stored as 8 bytes big endian
     min: IndexValue, // stored as 8 bytes, either the u64 in big endian or the first 8 bytes of the string
     max: IndexValue, // stored as 8 bytes, either the u64 in big endian or the first 8 bytes of the string
 }
 impl IndexEntry {
-    fn to_bytes(&self) -> [u8; 32]{
-        let mut bytes = [0; 32];
+    fn to_bytes(&self) -> [u8; 40]{
+        let mut bytes = [0; 40];
         bytes[0..8].copy_from_slice(&self.start_position.to_be_bytes());
-        bytes[8..16].copy_from_slice(&self.size.to_be_bytes());
-        self.min.write_bytes(&mut bytes[16..24]);
-        self.max.write_bytes(&mut bytes[24..32]);
+        bytes[8..16].copy_from_slice(&self.compressed_size.to_be_bytes());
+        bytes[16..24].copy_from_slice(&self.decompressed_size.to_be_bytes());
+        self.min.write_bytes(&mut bytes[24..32]);
+        self.max.write_bytes(&mut bytes[32..40]);
         bytes
     }
-    fn from_bytes(bytes: &[u8; 32], dtype: DType) -> IndexEntry {
+    fn from_bytes(bytes: &[u8; 40], dtype: &DType) -> IndexEntry {
         let start_position = u64::from_be_bytes(bytes[0..8].try_into().unwrap());
-        let size = u64::from_be_bytes(bytes[8..16].try_into().unwrap());
-        let min = IndexValue::from_bytes(&bytes[16..24], dtype);
-        let max = IndexValue::from_bytes(&bytes[24..32], dtype);
+        let compressed_size = u64::from_be_bytes(bytes[8..16].try_into().unwrap());
+        let decompressed_size = u64::from_be_bytes(bytes[16..24].try_into().unwrap());
+
+        let min = IndexValue::from_bytes(&bytes[24..32], dtype);
+        let max = IndexValue::from_bytes(&bytes[32..40], dtype);
         IndexEntry {
             start_position,
-            size,
+
+            compressed_size,
+            decompressed_size,
             min,
             max,
         }
@@ -106,15 +113,17 @@ mod tests {
     fn test_index_entry_to_bytes() {
         let entry = IndexEntry {
             start_position: 1,
-            size: 2,
-            min: IndexValue::Uint64(3),
+            compressed_size: 2,
+            decompressed_size: 3,
+            min: IndexValue::Uint64(4),
             max: IndexValue::from_dvalue(&DValue::String("longlonglong".to_string())),
         };
         let bytes = entry.to_bytes();
-        let expected: [u8; 32] = [
+        let expected: [u8; 40] = [
             0, 0, 0, 0, 0, 0, 0, 1,
             0, 0, 0, 0, 0, 0, 0, 2,
             0, 0, 0, 0, 0, 0, 0, 3,
+            0, 0, 0, 0, 0, 0, 0, 4,
             b'l', b'o', b'n', b'g', b'l', b'o', b'n', b'g',
         ];
         assert_eq!(bytes, expected);
@@ -241,7 +250,7 @@ pub fn write_data(
                 if get_dtype(&col) != col_writer.col.dtype {
                     return Err(anyhow!("Mismatched data type"));
                 }
-                write_dvalue_data(&mut col_state.buf, &col)?;
+                write_dvalue_data(&mut col_state.buf, &col);
                 col_state.min = get_min(&col_state.min, &col).clone();
                 col_state.max = get_max(&col_state.max, &col).clone();
             }
@@ -267,7 +276,8 @@ pub fn write_data(
 
             let index_entry = IndexEntry {
                 start_position: writer.position,
-                size: compressed_len as IndexSize,
+                compressed_size: compressed_len as IndexSize,
+                decompressed_size: buf_size as IndexSize,
                 min: IndexValue::from_dvalue(&col_state.min),
                 max: IndexValue::from_dvalue(&col_state.max),
             };
@@ -286,26 +296,58 @@ pub fn write_data(
     Ok(())
 }
 
-fn read_all(root_path: &PathBuf, table: &TableMetaData) -> Result<Vec<Vec<DType>>> {
+fn read_all(root_path: &PathBuf, table: &TableMetaData) -> Result<Vec<Vec<DValue>>> {
     let mut rows = Vec::new();
     let mut readers = create_readers(root_path, table, &table.columns)?;
-    let mut buffer = [0; 8];
     loop {
-        match readers[0].index_file.read_exact(&mut buffer) {
-            Ok(_) => {
-                // Successfully read 8 bytes, process the data as required
-                println!("{:?}", buffer);
+        let read_results: Result<Vec<IndexEntry>> = readers.iter_mut().map(|reader| {
+            let mut buffer = [0; 40];
+            reader.index_file.read_exact(&mut buffer)?;
+            let index_entry = IndexEntry::from_bytes(&buffer, &reader.col.dtype);
+            Ok(index_entry)
+        }).collect();
+        match read_results {
+            Ok(indexes) => {
+                read_all_from_block_group(&mut rows, &mut readers, indexes)?;
             },
-            Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
-                // Less than 8 bytes were left in the file or the file was empty
-                // Handle this case if necessary, otherwise break out of the loop
-                break;
-            },
-            Err(e) => return Err(e.into()), // Handle other errors
+            Err(e) => {
+                let error: Error = e.into();
+                match error.downcast_ref::<io::Error>() {
+                    Some(io_error) if io_error.kind() == ErrorKind::UnexpectedEof => {
+                        // Reached the end of the one of the index files
+                        break;
+                    }
+                    _ => return Err(error),
+                }
+            }
         }
     }
 
     return Ok(rows);
+}
+
+fn read_all_from_block_group(rows: &mut Vec<Vec<DValue>>, readers: &mut Vec<ColumnReader>, indexes: Vec<IndexEntry>) -> Result<()> {
+    // map over the readers, get all the column data for that reader
+    let block_group_rows = readers.iter_mut().enumerate().map(|(i, reader)| -> Result<()> {
+        let index_entry = &indexes[i];
+        // load the next block from the data file
+        let mut buffer = vec![0; index_entry.compressed_size as usize];
+        reader.data_file.seek(io::SeekFrom::Start(index_entry.start_position))?;
+        reader.data_file.read_exact(&mut buffer)?;
+        // decompress the data
+        let mut decompress_output = vec![0; index_entry.decompressed_size as usize];
+        lz4_flex::block::decompress_into(&buffer, &mut decompress_output)?;
+        // convert the bytes to DValues
+        let mut block_rows = Vec::new();
+        let mut bytes = decompress_output.as_slice(); 
+        while bytes.len() > 0 {
+            let (n_bytes, dvalue) = read_dvalue_data(bytes, &reader.col.dtype);
+            block_rows.push(dvalue);
+            bytes = &bytes[n_bytes..];
+        }
+        Ok(())
+    });
+    Ok(())
 }
 
 fn index_path(root_path: &PathBuf, table_name: &String, column_name: &String) -> PathBuf {
@@ -316,16 +358,38 @@ fn data_path(root_path: &PathBuf, table_name: &String, column_name: &String) -> 
     root_path.join(format!("{}.{}.data", table_name, column_name))
 }
 
-fn write_dvalue_data(bytes: &mut Vec<u8>, value: &DValue) -> Result<()> {
+fn write_dvalue_data(bytes: &mut Vec<u8>, value: &DValue) {
     match value {
         DValue::String(s) => {
-            let null_terminated = format!("{}\0", s);
-            bytes.extend_from_slice(&null_terminated.as_bytes());
+            // use length prefixed format, with a u32 for string length
+            if s.len() > u32::MAX as usize {
+                panic!("String too long");
+            }
+            let length_bytes: [u8; 4] = (s.len() as u32).to_be_bytes();
+            bytes.extend_from_slice(&length_bytes);
+            bytes.extend_from_slice(s.as_bytes());
         }
         DValue::Uint64(u) => {
             bytes.extend_from_slice(&u.to_be_bytes());
         }
     };
-    Ok(())
+}
+
+fn read_dvalue_data(bytes: &[u8], dtype: &DType) -> (usize, DValue) {
+    match dtype {
+        DType::String => {
+            let length_bytes: [u8; 4] = bytes[0..4].try_into().unwrap();
+            let length = u32::from_be_bytes(length_bytes) as usize;
+            let s_bytes = &bytes[4..(4 + length)];
+            // I double checked the docs, and the inverse to as_bytes is *actually* from_utf8
+            let s = String::from_utf8(s_bytes.to_vec()).unwrap();
+            (4 + length, DValue::String(s))
+        }
+        DType::Uint64 => {
+            let u_bytes: [u8; 8] = bytes[0..8].try_into().unwrap();
+            let u = u64::from_be_bytes(u_bytes);
+            (8, DValue::Uint64(u))
+        }
+    }
 }
 
